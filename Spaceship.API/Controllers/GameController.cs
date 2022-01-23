0b7d86b1d86db6d4659cc1be01998082c943538b
Roko <rokokovac98@gmail.com>
2022-01-23 22:11:28 +0100

    Add NewGame Protocol API endpoint

---
+++ Spaceship.API/Controllers/GameController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Spaceship.API.Models;
+using Spaceship.DataAccess.Entities;
+using Spaceship.DataAccess.Interfaces;
+
+namespace Spaceship.API.Controllers
+{
+    [Route("/spaceship/protocol/game")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly IGameRepository gameRepository;
+        private readonly IConfiguration config;
+
+        public GameController(IGameRepository gameRepository, IConfiguration config)
+        {
+            this.gameRepository = gameRepository;
+            this.config = config;
+        }
+
+        [HttpPost("new")]
+        public ActionResult NewGame(GameNewDTO gameNewDto)
+        {
+            var game = new Game
+            {
+                PlayerId = config["PlayerId"],
+                PlayerName = config["PlayerName"],
+                OpponentId = gameNewDto.UserId,
+                OpponentName = gameNewDto.FullName,
+                OpponentHostname = gameNewDto.SpaceshipProtocol.Hostname,
+                OpponentPort = gameNewDto.SpaceshipProtocol.Port
+            };
+
+            var gameId = gameRepository.CreateGame(game);
+
+            var createdGame = new GameCreatedDTO
+            {
+                UserId = config["PlayerId"],
+                FullName = config["PlayerName"],
+                GameId = $"match-{gameId}",
+                Starting = game.Starting
+            };
+
+            var uri = $"{config["UserAPI"]}/match-{gameId}";
+
+            return Created(uri, createdGame);
+        }
+    }
+}
---
+++ Spaceship.API/Models/GameCreatedDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Spaceship.API.Models
+{
+    public class GameCreatedDTO
+    {
+        public string UserId { get; set; }
+        public string FullName { get; set; }
+        public string GameId { get; set; }
+        public string Starting { get; set; }
+    }
+}
