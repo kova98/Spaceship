cb5bfa8d40294457e58ab252b805a03a7cfac25a
Roko <rokokovac98@gmail.com>
2022-01-24 02:42:17 +0100

    Add Ships helper

---
+++ Spaceship.API/Infrastructure/Ships.cs
@@ -0,0 +1,89 @@
+﻿namespace Spaceship.ProtocolAPI.Infrastructure
+{
+    public static class Ships
+    {
+        public static int[,] GetShip(ShipType type, ShipOrientation orientation)
+        {
+            var ship = type switch
+            {
+                ShipType.Winger => Winger,
+                ShipType.Angle => Angle,
+                ShipType.AClass => AClass,
+                ShipType.BClass => BClass,
+                ShipType.SClass => SClass,
+            };
+
+            for (int i = 0; i < (int)orientation; i++)
+            {
+                ship = RotateClockwise(ship);
+            }
+
+            return ship;
+        }
+
+        private static int[,] Winger => new int[,] {
+            { 1, 0, 1 },
+            { 1, 0, 1 },
+            { 0, 1, 0 },
+            { 1, 0, 1 },
+            { 1, 0, 1 }};
+
+        private static int[,] Angle => new int[,] { 
+            { 1, 0, 0 },
+            { 1, 0, 0 },
+            { 1, 0, 0 },
+            { 1, 1, 1 },
+            { 0, 0, 0 }};
+
+        private static int[,] AClass => new int[,] {
+            { 0, 1, 0 },
+            { 1, 0, 1 },
+            { 1, 1, 1 },
+            { 1, 0, 1 },
+            { 0, 0, 0 }};
+
+        private static int[,] BClass => new int[,] {
+            { 1, 1, 0 },
+            { 1, 0, 1 },
+            { 1, 1, 0 },
+            { 1, 0, 1 },
+            { 1, 1, 0 }};
+
+        private static int[,] SClass => new int[,] {
+            { 0, 1, 1 },
+            { 1, 0, 0 },
+            { 0, 1, 1 },
+            { 0, 0, 1 },
+            { 1, 1, 0 }};
+
+        private static int[,] RotateClockwise(int[,] src)
+        {
+            int width;
+            int height;
+            int[,] dst;
+
+            width = src.GetUpperBound(0) + 1;
+            height = src.GetUpperBound(1) + 1;
+            dst = new int[height, width];
+
+            for (int row = 0; row < height; row++)
+            {
+                for (int col = 0; col < width; col++)
+                {
+                    int newRow;
+                    int newCol;
+
+                    newRow = col;
+                    newCol = height - (row + 1);
+
+                    dst[newCol, newRow] = src[col, row];
+                }
+            }
+
+            return dst;
+        }
+    }
+
+    public enum ShipType { Winger, Angle, AClass, BClass, SClass }
+    public enum ShipOrientation { Top, Left, Bottom, Right }
+}
---
+++ Spaceships.Tests.Unit/ProtocolAPI/Infrastructure/ShipsTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using Spaceship.ProtocolAPI.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Spaceships.Tests.Unit.ProtocolAPI.Infrastructure
+{
+    public class ShipsTests
+    {
+        [Theory]
+        [InlineData(ShipType.Winger, "1,0,1,1,0,1,0,1,0,1,0,1,1,0,1")]
+        [InlineData(ShipType.Angle, "1,0,0,1,0,0,1,0,0,1,1,1,0,0,0")]
+        [InlineData(ShipType.AClass, "0,1,0,1,0,1,1,1,1,1,0,1,0,0,0")]
+        [InlineData(ShipType.BClass, "1,1,0,1,0,1,1,1,0,1,0,1,1,1,0")]
+        [InlineData(ShipType.SClass, "0,1,1,1,0,0,0,1,1,0,0,1,1,1,0")]
+        void GetShip_ReturnsCorrectShip(ShipType shipType, string shipAsArray)
+        {
+            var ship = Ships.GetShip(shipType, ShipOrientation.Top);
+            var flattened = ship.Cast<int>().ToArray();
+            var shipString = string.Join(',', flattened);
+
+            shipString.Should().Be(shipAsArray);
+        }
+
+        //    Top          Left       Bottom         Right  
+        //  1, 0, 0   0, 1, 1, 1, 1   0, 0, 0    0, 0, 0, 1, 0 
+        //  1, 0, 0   0, 1, 0, 0, 0   1, 1, 1    0, 0, 0, 1, 0     
+        //  1, 0, 0   0, 1, 0, 0, 0   0, 0, 1    1, 1, 1, 1, 0   
+        //  1, 1, 1                   0, 0, 1
+        //  0, 0, 0                   0, 0, 1    
+
+        [Theory]
+        [InlineData(ShipOrientation.Top, "1,0,0,1,0,0,1,0,0,1,1,1,0,0,0")]
+        [InlineData(ShipOrientation.Right, "0,1,1,1,1,0,1,0,0,0,0,1,0,0,0")]
+        [InlineData(ShipOrientation.Bottom, "0,0,0,1,1,1,0,0,1,0,0,1,0,0,1")]
+        [InlineData(ShipOrientation.Left, "0,0,0,1,0,0,0,0,1,0,1,1,1,1,0")]
+        void GetShip_ReturnsCorrectOrientation(ShipOrientation shipOrientation, string shipAsArray)
+        {
+            var ship = Ships.GetShip(ShipType.Angle, shipOrientation);
+            var flattened = ship.Cast<int>().ToArray();
+            var shipString = string.Join(',', flattened);
+
+            shipString.Should().Be(shipAsArray);
+        }
+    }
+}
