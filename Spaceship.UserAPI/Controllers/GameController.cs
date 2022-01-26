be4c19cde68c4baa02f283f19609afbf75d41428
Roko <rokokovac98@gmail.com>
2022-01-26 22:24:39 +0100

    Add User API fire endpoint

--- Spaceship.UserAPI/Controllers/GameController.cs
+++ Spaceship.UserAPI/Controllers/GameController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Spaceship.DataAccess.Entities;
+using Spaceship.DataAccess.Interfaces;
+using Spaceship.Domain;
+using Spaceship.UserAPI.Models;
 
 namespace Spaceship.UserAPI.Controllers
 {
@@ -7,10 +11,52 @@ namespace Spaceship.UserAPI.Controllers
     [ApiController]
     public class GameController : ControllerBase
     {
-        [HttpPut("{gameId}/fire")]
-        public ActionResult Fire(string gameId)
+        private IGameRepository gameRepository;
+        private IConfiguration config;
+
+        public GameController(IGameRepository gameRepository, IConfiguration config)
         {
-            return null;
+            this.gameRepository = gameRepository;
+            this.config = config;
+        }
+
+        [HttpPut("{gameId}/fire")]
+        public ActionResult Fire(FireDTO fireDto, string gameId)
+        {
+            var parsedGameId = ParseId(gameId);
+            var game = gameRepository.GetGame(parsedGameId);
+            var gameManager = new GameManager(gameRepository, game);
+
+            var shots = gameManager.Fire(fireDto.Salvo);
+
+            var fireResponseDto = new FireResponseDTO
+            {
+                Salvo = ShotsToDictionary(shots),
+                Game = new GameInfo { PlayerTurn = game.PlayerTurn }
+            };
+
+            return Ok(fireResponseDto);
+        }
+
+        private Dictionary<string, string> ShotsToDictionary(List<Shot> shots)
+        {
+            var dictionary = new Dictionary<string, string>();
+            foreach (var shot in shots)
+            {
+                var x = shot.Location.X.ToString("X");
+                var y = shot.Location.Y.ToString("X");
+                var location = $"{x}x{y}";
+                dictionary.Add(location, shot.Status.ToString().ToLower());
+            }
+
+            return dictionary;
+        }
+
+        private long ParseId(string gameId)
+        {
+            var idString = gameId.Split('-').LastOrDefault();
+            long.TryParse(idString, out long id);
+            return id;
         }
     }
 }
--- Spaceships.Tests.Unit/UserAPI/Controllers/GameControllerTests.cs
+++ Spaceships.Tests.Unit/UserAPI/Controllers/GameControllerTests.cs
@@ -2,11 +2,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Moq;
-using Spaceship.API.Controllers;
 using Spaceship.DataAccess.Entities;
 using Spaceship.DataAccess.Interfaces;
-using Spaceship.ProtocolAPI.Infrastructure;
-using Spaceship.ProtocolAPI.Models;
+using Spaceship.UserAPI.Controllers;
+using Spaceship.UserAPI.Models;
+using Spaceships.Tests.Unit.Helpers;
 using System;
 using System.Linq;
 using Xunit;
@@ -15,5 +15,44 @@ namespace Spaceships.Tests.Unit.UserAPI.Controllers
 {
     public class GameControllerTests
     {
+        [Fact]
+        void Fire_RegistersHitsAndReturnsCorrectShotStatuses()
+        {
+            const int Ship = 1;
+            const int Hit = -1;
+            var game = GameHelper.GetGameWithFieldSetTo((0, 1, Ship), (0, 2, Hit), (1, 3, Hit), (0, 4, Ship));
+            var gameRepoMock = new Mock<IGameRepository>();
+            gameRepoMock.Setup(x => x.GetGame(It.IsAny<long>())).Returns(game);
+            var controller = new GameController(gameRepoMock.Object, Mock.Of<IConfiguration>());
+            var fireDto = new FireDTO { Salvo = new string[] { "0x0", "0x1", "0x2", "0x4" } };
+
+            var result = (OkObjectResult)controller.Fire(fireDto, "");
+            var responseDto = result.Value as FireResponseDTO;
+
+            responseDto.Should().BeOfType<FireResponseDTO>();
+            responseDto.Salvo.Count.Should().Be(4);
+            responseDto.Salvo["0x0"].Should().Be("miss", "the field is empty");
+            responseDto.Salvo["0x1"].Should().Be("hit", "there is a ship on the field");
+            responseDto.Salvo["0x2"].Should().Be("miss", "the field is already hit");
+            responseDto.Salvo["0x4"].Should().Be("kill", "it is the last hit field of the ship");
+        }
+
+        [Fact]
+        void Fire_RespondsWithCorrectPlayerTurn()
+        {
+            var game = GameHelper.GetGameWithFieldSetTo((0, 1, 1));
+            game.PlayerId = "player-1";
+            game.OpponentId = "opponent-1";
+            game.PlayerTurn = "opponent-1";
+            var gameRepoMock = new Mock<IGameRepository>();
+            gameRepoMock.Setup(x => x.GetGame(It.IsAny<long>())).Returns(game);
+            var controller = new GameController(gameRepoMock.Object, Mock.Of<IConfiguration>());
+            var fireDto = new FireDTO { Salvo = new string[] { "0x0" } };
+
+            var result = (OkObjectResult)controller.Fire(fireDto, "");
+            var responseDto = result.Value as FireResponseDTO;
+
+            responseDto.Game.PlayerTurn.Should().Be("player-1");
+        }
     }
 }
